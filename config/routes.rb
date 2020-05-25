6607b4daa6f783779765414b2f1d0dd302de4603
Caleb Woods <woodscaleb@gmail.com>
2020-05-25 10:06:22 -0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :newsletters
+  comfy_route :cms_admin, path: "/admin"
+  # Ensure that this route is defined last
+  comfy_route :cms, path: "/"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+end
